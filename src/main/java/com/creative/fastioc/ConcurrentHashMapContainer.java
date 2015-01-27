d2d9c5cdf476ea4d425ec3eebe73ac55dd57109a
vudangngoc <vudangngoc@gmail.com>
2015-01-27 09:05:36 +0700

    improve ConcurrentHashMapContainer by Chad3F
    improved by Chad3F 
    http://www.codeproject.com/Tips/868625/Simple-IoC-Container-Base-on-ConcurrentHashMap?msg=4988923#xx4988923xx

--- src/main/java/com/creative/fastioc/ConcurrentHashMapContainer.java
+++ src/main/java/com/creative/fastioc/ConcurrentHashMapContainer.java
@@ -6,36 +6,25 @@ public class ConcurrentHashMapContainer implements ITypeCheckContainer {
 
 	private ConcurrentHashMap<Class<?>, Object> container = new ConcurrentHashMap<Class<?>, Object>();
 
-
 	@Override
 	public <K, V extends K> boolean regit(Class<K> key, V value) {
-		container.put(key, value);
-		return true;
+		return container.put(key, value) == null;
 	}
 
 	@Override
 	public <K, V extends K> boolean regit(Class<K> key, Class<V> value)
 			throws Exception {
-		V object = null;
+		V object;
 		try {
 			object = ReflectHelper.createObject(this,value);
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			throw e;
 		}
-		if(container.containsKey(key)){
-			container.remove(key);
-			container.put(key, object);
-			return false;
-		}
-		container.put(key, object);
-		return true;
+		return (container.put(key, object) == null);
 	}
 
 	@Override
 	public <K, V extends K> V resolve(Class<K> keyObject) {
-		
 		return (V) container.get(keyObject);
 	}
-
 }
